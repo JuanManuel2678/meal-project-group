d87a88fd3a2f5751691ff5b7840480eb1f353cfe
Juan_manuel <jaredjuan71@gmail.com>
2024-07-11 12:56:59 -0400

    project

---
+++ src/assets/components/MealDitails/Details.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+    <div>
+      <h1>detalles de  la comida </h1>
+    </div>
+  )
+}
+
+export default Details
---
+++ src/assets/components/MealList/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      <h1>lista de comidas</h1>
+    </div>
+  )
+}
+
+export default Main
