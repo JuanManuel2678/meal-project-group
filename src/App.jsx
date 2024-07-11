fed6e1b4469e5074d1e8f6a3a0ac342bce78c276
Javier <javier.jc942@gmail.com>
2024-07-11 19:53:19 -0400

    Details

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Details from './components/MealDitails/Details'
+const App = () => {
+  return (
+    <div>
+      <h1>app</h1>
+      <Details />
+    </div>
+  )
+}
+
+export default App
