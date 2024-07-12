cc4187cbd2b4e28549140cb7a8f0b9cd09659501
Juan_manuel <jaredjuan71@gmail.com>
2024-07-12 15:22:19 -0400

    compnentes en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { RightSide } from './components/Categori/RightSide'
-import Details from './components/MealDitails/Details'
-
-
+import React from "react";
+import { RightSide } from "./components/Categori/RightSide";
+import Details from "./components/MealDitails/Details";
+import { Main } from "./components/Meals/Main";
 
 const App = () => {
   return (
-   
-      <div className='app_container'>
+    <>
+      <div className="app_container">
         <RightSide />
-        <Details />
+        <Main/>
       </div>
-  
+      <Details />
+    </>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
