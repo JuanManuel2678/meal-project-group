7b4a3c2d5493fbdf512ef64224d43a3985188f91
Juan_manuel <jaredjuan71@gmail.com>
2024-07-12 10:18:57 -0400

    categori component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { RightSide } from './components/Categori/RightSide'
-import { Main } from './components/Meals/Main'
+
 
 const App = () => {
   return (
    
       <div className='app_container'>
-      <RightSide/>
-      <Main/>
+ 
       </div>
   
   )
